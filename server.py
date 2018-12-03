0096c545b25e3154908f0b30a58b6b18de5c52ea
palsch <pal03377@gmail.com>
2018-12-03 20:52:33 +0100

    add title and description and reqrite the questions in English

--- server.py
+++ server.py
@@ -2,31 +2,35 @@ from flask import *
 
 app = Flask(__name__)
 
+title = "Survey Test"
+
+description = "This is just a simple test."
+
 questions = [
     {
         "type": "text-one-line", 
-        "title": "Sag mal was.", 
-        "placeholder": "Tipp: Lorem ipsum", 
+        "title": "Say something.", 
+        "placeholder": "Hint: Lorem ipsum", 
         "name": "test1"
     }, 
     {
         "type": "text-one-line", 
-        "title": "Sag mal noch was.",
-        "placeholder": "Tipp: Dolor sit", 
+        "title": "Say something again.",
+        "placeholder": "Hint: Dolor sit", 
         "name": "test2"
     },
     {
         "type": "multiple-choice", 
-        "title": "Wann wird dieses Projekt fertig?",
-        "placeholder": "Auswahlfrage", 
+        "title": "When will this project be completed?",
+        "placeholder": "Multiple choice question", 
         "name": "test3", 
-        "options": ["heute", "morgen", "in 3 Tagen", "nächste Woche", "irgendwann"]
+        "options": ["today", "tomorrow", "in 3 days", "next week", "at some point in time"]
     }
 ]
 
 @app.route("/")
 def index():
-    return render_template("survey.html", questions=questions)
+    return render_template("survey.html", title=title, description=description, questions=questions)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
