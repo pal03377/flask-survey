038b58aa896eef94db83f5b9a3384f798a488ccb
palsch <pal03377@gmail.com>
2018-12-04 21:56:57 +0100

    404 page

--- server.py
+++ server.py
@@ -237,5 +237,10 @@ def access_denied(error):
     return error.description
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html")
+
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5000, debug=True)
